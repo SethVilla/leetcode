91b34e16e85528635be55a7a6ba3af16da253e54
Seth Villa <sethvilla13@gmail.com>
2021-04-27 18:09:45 -0400

    leetcode refactor

--- easy/TwoSum.js
+++ easy/TwoSum.js
@@ -9,7 +9,7 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
+const twoSum = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) {
@@ -18,3 +18,19 @@ var twoSum = function (nums, target) {
     }
   }
 };
+
+//Alternative solution for fun
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ const twoSum = (nums, target) => nums.reduce((tuple, next, index) => {
+  const arr1 = nums.slice(index + 1)
+  const targetIndex = arr1.findIndex((value) => next + value === target)
+  
+  if(targetIndex > -1){
+       tuple = [index, targetIndex + index + 1]
+  }
+  return tuple
+}, [])
