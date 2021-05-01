7af1d6871e3af904cd2fe712d520b8a1941067ec
Seth Villa <sethvilla13@gmail.com>
2021-04-30 22:46:00 -0400

    plus one

---
+++ easy/RemoveElement.js
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/remove-element/
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+const removeElement = (nums, val) => {
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+    }
+  }
+
+  return nums.length;
+};
