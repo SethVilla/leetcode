ba6aa22ee060ea127da9b320533b686719acad5b
Seth Villa <sethvilla13@gmail.com>
2024-03-04 22:23:49 -0800

    extra credit 141

---
+++ java/206-Reverse-Linked-List/Main.java
@@ -0,0 +1,28 @@
+// 206. Reverse Linked List
+// https://leetcode.com/problems/reverse-linked-list/description/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        return prev;
+    }
+}
---
+++ java/234-Palindrome-Linked-List/Main.java
@@ -0,0 +1,44 @@
+// 234. Palindrome Linked List
+// https://leetcode.com/problems/palindrome-linked-list/description/
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode secondHalf = reverseList(slow);
+
+        while (secondHalf != null) {
+            if (head.val != secondHalf.val) {
+                return false;
+            }
+            head = head.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        return prev;
+    }
+}
