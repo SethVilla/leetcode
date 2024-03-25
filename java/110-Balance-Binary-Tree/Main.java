b584a30f9b1d80d2893cd9b1e828d63da4cb043c
Seth Villa <sethvilla13@gmail.com>
2024-03-25 12:14:28 -0700

    extra credit 6

---
+++ java/110-Balance-Binary-Tree/Main.java
@@ -0,0 +1,37 @@
+// 110 Balance Tree
+// https://leetcode.com/problems/balanced-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return getHeight(root) != -1;
+    }
+
+    private int getHeight(TreeNode node) {
+        if (node == null) return 0;
+
+        int leftHeight = getHeight(node.left);
+        if (leftHeight == -1) return -1;
+
+        int rightHeight = getHeight(node.right);
+        if (rightHeight == -1) return -1;
+
+        if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
